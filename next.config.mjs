9a33b358418c444715c78dc96e5b7a4e4b048935
Sigurd Berg <sigurd@sigurdberg.com>
2026-02-19 13:48:58 +0100

    ny

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,27 @@
-import createMDX from "@next/mdx";
+import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  transpilePackages: ["next-mdx-remote"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.google.com",
+        pathname: "**",
+      },
+    ],
+  },
+  sassOptions: {
+    compiler: "modern",
+    silenceDeprecations: ["legacy-js-api"],
+  },
 };
-
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-});
 
 export default withMDX(nextConfig);
