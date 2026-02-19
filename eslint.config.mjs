ac538b82294a8f03f3718d9c1cef8e4dffd2969b
Sigurd Berg <sigurd@sigurdberg.com>
2026-02-19 13:20:58 +0100

    Fix ESLint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,14 @@ const eslintConfig = [
     "plugin:prettier/recommended",
     "plugin:mdx/recommended",
   ),
+  {
+    rules: {
+      "react/jsx-no-undef": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
