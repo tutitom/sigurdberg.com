5873d9f47f8055adcf9edde9a1aab9f85d4b2613
Sigurd Berg <sigurd@sigurdberg.com>
2026-02-19 13:32:47 +0100

    Fix prettier formatting on cover

--- components/ui/cover.tsx
+++ components/ui/cover.tsx
@@ -9,7 +9,13 @@ interface CoverProps {
 export default function Cover({ src, alt, caption }: CoverProps) {
   return (
     <figure className="my-8">
-      <Image src={src} alt={alt} width={1200} height={630} className="w-full rounded-lg" />
+      <Image
+        src={src}
+        alt={alt}
+        width={1200}
+        height={630}
+        className="w-full rounded-lg"
+      />
       {caption && (
         <figcaption className="text-center text-sm text-gray-500 mt-2">
           {caption}
