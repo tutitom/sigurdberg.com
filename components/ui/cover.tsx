297f7943525f5dc8d170fedaff7ab70f8fa4c57d
Sigurd Berg <sigurd@sigurdberg.com>
2026-02-19 13:31:54 +0100

    Fix Cover component and prettier

--- components/ui/cover.tsx
+++ components/ui/cover.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CoverProps {
   src: string;
   alt: string;
@@ -7,7 +9,7 @@ interface CoverProps {
 export default function Cover({ src, alt, caption }: CoverProps) {
   return (
     <figure className="my-8">
-      <img src={src} alt={alt} className="w-full rounded-lg" />
+      <Image src={src} alt={alt} width={1200} height={630} className="w-full rounded-lg" />
       {caption && (
         <figcaption className="text-center text-sm text-gray-500 mt-2">
           {caption}
